62aa0b5fc7f3cb7fd6ec80af51777907348b8a9b
Mayank Sinha <mayanksinha222000@gmail.com>
2023-03-02 23:57:04 +0530

    Add files via upload

---
+++ deletion.java
@@ -0,0 +1,10 @@
+package com.Learning_Java;
+
+public class deletion {
+    public static void main(String[] args) {
+        String s = "Mayank Sinha";
+        System.out.println("Actual String: " + s);
+        String newS = s.replace(" ", "");
+        System.out.println("Resultant String: " + newS);
+    }
+}
